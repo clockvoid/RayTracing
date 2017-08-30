0fb0be461850f8376a228e176c3a1a37088c6599
clockvoid <hidenari_1@yahoo.co.jp>
2017-08-30 23:08:36 +0900

    add: implement of Vector3 class

--- src/main/kotlin/Vector.kt
+++ src/main/kotlin/Vector.kt
@@ -1,10 +1,31 @@
 
 interface Vector {
-	fun innerProduct(): Double
-	operator fun plus(arg0: Vector): Vector
-	operator fun minus(arg0: Vector): Vector
+
+	val x: Double
+	val y: Double
+	val z: Double
+
+	public infix fun dot(arg0: Vector): Double
+	public infix fun cross(arg0: Vector): Vector
+	public operator fun plus(arg0: Vector): Vector
+	public operator fun minus(arg0: Vector): Vector
+	public fun abs(): Double
+	public operator fun times(arg0: Double): Vector
+	public operator fun div(arg0: Double): Vector
 }
 
-class Vector3(val x: Double, val y: Double, val z: Double) {
+class Vector3(val arg0: Double, val arg1: Double, val arg2: Double) : Vector{
+
+	override val x: Double = arg0
+	override val y: Double = arg1
+	override val z: Double = arg2
+
+	override public infix fun dot(vec: Vector): Double = vec.x * this.x + vec.y * this.y + vec.z * this.z
+	override public infix fun cross(arg0: Vector): Vector = Vector3(this.y * arg0.z - this.z * arg0.y, this.z * arg0.x - this.x * arg0.z, this.x * arg0.y - this.y * arg0.x)
+	override public operator fun plus(arg0: Vector): Vector = Vector3(arg0.x + this.x, arg0.y + this.y, arg0.z + this.z)
+	override public operator fun minus(arg0: Vector): Vector = Vector3(this.x - arg0.x, this.y - arg0.y, this.z - arg0.z)
+	override public fun abs(): Double = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+	override public operator fun times(arg0: Double): Vector = Vector3(this.x * arg0, this.y * arg0, this.z * arg0)
+	override public operator fun div(arg0: Double): Vector = Vector3(this.x / arg0, this.y / arg0, this.z / arg0)
 
 }
