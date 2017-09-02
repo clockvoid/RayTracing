d11bc31ffeaae8b6dd4c813350a8335999c9d91d
clockvoid <hidenari_1@yahoo.co.jp>
2017-09-02 22:13:54 +0900

    modified: App.kt

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,15 @@
 
+import vector.Vector
 import vector.Vector3
+
+fun calc(arg0: Vector) {
+	println(arg0.abs())
+}
 
 fun main(args: Array<String>) {
 	println("Hello, world")
 	val vec = Vector3(1.0, 2.0, 3.0)
+	calc(vec)
 	println(vec.toString())
-	println(vec.abs())
+	println(vec::class)
 }
