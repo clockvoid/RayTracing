d873589be07b229887b1bb6def4ba8fd27bcf67c
clockvoid <hidenari_1@yahoo.co.jp>
2017-09-02 17:51:44 +0900

    fix: readability

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,15 +5,31 @@ import color.Color
 import color.Color3
 import java.io.File
 
+infix fun Double.pow(arg0: Double): Double = Math.pow(this, arg0)
+
 val Rx: Double = 256.0
 val Ry: Double = 256.0
 
 val size: Double = 5.0
 
-fun main(args: Array<String>) {
-    val cord: Array<Array<Vector3>> = Array(Rx.toInt(), { l -> Array(Ry.toInt(), { t -> Vector3(size / Rx * (0.5 + l) - size / 2 , size / Ry * (0.5 + t) - size / 2, 10.0) }) })
+val a: Vector = Vector3(0.0, 0.0, 4.0)
 
-    val colorCord = cord.map({ xs -> xs.map({ vec -> if ((Math.pow(((Vector3(0.0, 0.0, 4.0) - vec) / ((Vector3(0.0, 0.0, 4.0) - vec).abs())) dot vec, 2.0) - (Math.pow(vec.abs(), 2.0) - 1.0)) >= 0.0) Color3(255, 0, 0) else Color3(255, 255, 255) }) })
+fun main(args: Array<String>) {
+    val cord = List(Rx.toInt(), {
+        l -> List(Ry.toInt(), {
+            t -> Vector3(size / Rx * (0.5 + l) - size / 2 , size / Ry * (0.5 + t) - size / 2, 10.0)
+        })
+    })
+
+    val colorCord = cord.map {
+        xs -> xs.map {
+            vec -> if (((((a - vec) / (a - vec).abs()) dot vec).pow(2.0) - (vec.abs().pow(2.0) - 1.0)) >= 0.0) {
+                Color3(255, 0, 0)
+            } else {
+                Color3(255, 255, 255)
+            }
+        }
+    }
 
     val picFile = File("pic.ppm").absoluteFile
 
