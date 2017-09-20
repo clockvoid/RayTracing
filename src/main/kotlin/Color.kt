5825579d950def41e91e3e4970921c6ccf248e1a
clockvoid <hidenari_1@yahoo.co.jp>
2017-09-20 17:50:48 +0900

    add: FColor

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -15,13 +15,31 @@ class Color3(val arg0: Int, val arg1: Int, val arg2: Int) : Color {
 
     override fun toString(): String {
         val builder = StringBuilder()
-        builder.append(this.red.toString())
+        builder.append(red.toString())
         builder.append(" ")
-        builder.append(this.green.toString())
+        builder.append(green.toString())
         builder.append(" ")
-        builder.append(this.blue.toString())
+        builder.append(blue.toString())
         builder.append("\n")
         return builder.toString()
     }
 
+}
+
+class FColor(arg0: Double, arg1: Double, arg2: Double) {
+	val red: Double = arg0
+	val green: Double = arg1
+	val blue: Double = arg2
+
+	fun toColor(): Color = Color3(red.toInt(), green.toInt(), blue.toInt())
+	override fun toString(): String {
+	    val builder = StringBuilder()
+        builder.append(red.toString())
+        builder.append(" ")
+        builder.append(green.toString())
+        builder.append(" ")
+        builder.append(blue.toString())
+        builder.append("\n")
+        return builder.toString()
+	}
 }
