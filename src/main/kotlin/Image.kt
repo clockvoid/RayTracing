8ac7ef4f1a3e439581a111b3556de53630fd8071
Lucciola <hidenari_1@yahoo.co.jp>
2017-10-02 17:08:40 +0900

    add: supprt for windows and intellij

--- src/main/kotlin/Image.kt
+++ src/main/kotlin/Image.kt
@@ -11,7 +11,7 @@ interface Image {
 }
 
 class Ppm(val arg1: String) : Image {
-    override val file = File(arg1).absoluteFile
+    override val file = File(arg1.replace(":", "")).absoluteFile
 
     override public fun save(arg0: String): Unit {
         file.writeText(arg0)
